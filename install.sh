0f4f0d1f5d65a9dff735b5fc836f4e0122581dfd
chxbca <272141735@qq.com>
2020-02-05 22:40:04 +0800

    修改一键安装脚本

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 echo 'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ bionic main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ bionic-updates main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ bionic-backports main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ bionic-security main restricted universe multiverse' > sources.list
-sudo rm /etc/apt/sources.list
-sudo mv sources.list /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ bionic-security main restricted universe multiverse' > ~/sources.list
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo mv ~/sources.list /etc/apt/sources.list
+
 sudo apt-get update
 sudo apt-get install -y \
     apt-transport-https \
@@ -11,22 +12,29 @@ sudo apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-# 安装 GPG 证书
+    
+# 安装 docker-ce
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-# 新增软件源信息
 sudo add-apt-repository "deb [arch=armhf] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo echo '{
-  "registry-mirrors": [
-    "https://registry.docker-cn.com"
-  ]
-}' >> daemon.json
-sudo mv daemon.json /etc/docker/
-sudo rm /usr/share/maven/conf/settings.xml 
-sudo mv settings /usr/share/maven/conf
+
+sudo mkdir -p /etc/docker
+
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://uqvzue7x.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+# 安装kubeadm kubelet kubectl
 curl -fsSL 'https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg' | sudo apt-key add - 
-sudo echo 'deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main' >> kubernetes.list
-sudo mv kubernetes.list /etc/apt/sources.list.d/
+
+sudo tee /etc/apt/sources.list.d/kubernetes.list <<-'EOF'
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubeadm kubelet kubectl
